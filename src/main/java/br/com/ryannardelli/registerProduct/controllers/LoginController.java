732a900b93602207c56858327740593458fbdfce
Ryan <ryannarelli12@gmail.com>
2025-03-05 19:03:07 -0300

    enviando novos arquivos e pastas

---
+++ src/main/java/br/com/ryannardelli/registerProduct/controllers/LoginController.java
@@ -0,0 +1,14 @@
+package br.com.ryannardelli.registerProduct.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class LoginController {
+    @GetMapping("/login")
+    public ModelAndView login() {
+        ModelAndView mv = new ModelAndView("login/login");
+        return mv;
+    }
+}
