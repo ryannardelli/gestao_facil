acaa2bc00096838a79c340b5ea862cb7e436859d
Ryan <ryannarelli12@gmail.com>
2025-03-11 21:01:43 -0300

    enviando ajuste

--- src/main/java/br/com/ryannardelli/registerProduct/controllers/ProviderController.java
+++ src/main/java/br/com/ryannardelli/registerProduct/controllers/ProviderController.java
@@ -1,6 +1,5 @@
 package br.com.ryannardelli.registerProduct.controllers;
 
-import br.com.ryannardelli.registerProduct.Models.Product;
 import br.com.ryannardelli.registerProduct.Models.Provider;
 import br.com.ryannardelli.registerProduct.dto.RequestNewProvider;
 import br.com.ryannardelli.registerProduct.repositories.ProviderRepository;
@@ -10,12 +9,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/provider")
@@ -38,6 +39,20 @@ public class ProviderController {
         ModelAndView mv = new ModelAndView("provider/new");
         mv.addObject("requestNewProvider", new RequestNewProvider());
         return mv;
+    }
+
+    @GetMapping("/{id}")
+    public ModelAndView showProduct(@PathVariable long id) {
+        Optional<Provider> optional = this.providerRepository.findById(id);
+
+        if(optional.isPresent()) {
+            Provider provider = optional.get();
+            ModelAndView mv = new ModelAndView("provider/show");
+            mv.addObject("provider", provider);
+            return mv;
+        } else {
+            return new ModelAndView("redirect:/provider");
+        }
     }
 
     @PostMapping("")
